32139458440e72c2cc949b9d5ac9c50e43c71271
karamala surya <suryakaramala63@gmail.com>
2025-07-21 22:26:09 +0530

    this a sample program

--- git.py
+++ git.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 print("helllo world  baba u must about the anaconda")
 print("its interstellargit ")
+=======
+print("helllo world")
+>>>>>>> parent of 005854f (it just a sample program)
